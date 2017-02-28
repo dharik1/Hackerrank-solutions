cfda260d0bbec058366a95c62118d184996d136e
dharik1 <dharikahamed1@gmail.com>
2017-02-28 15:33:36 +0530

    Create Circular Array Rotation.cpp

---
+++ Circular Array Rotation.cpp
@@ -0,0 +1,46 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    int q;
+    cin >> n >> k >> q;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+       cin >> a[(a_i+k)%n];
+    }
+    for(int a0 = 0; a0 < q; a0++){
+        int m;
+        cin >> m;
+        cout<<a[m]<<endl;
+    }
+   /* for(int i=0;i<n;i++)
+        {
+        cout<<a[i]<<" ";
+    }*/
+    return 0;
+}
